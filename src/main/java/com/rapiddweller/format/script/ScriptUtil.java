de3180def5121c5f8ecb789335d3b426e8c079b2
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-09-29 08:38:25 +0200

    Improved code quality

--- src/main/java/com/rapiddweller/format/script/ScriptUtil.java
+++ src/main/java/com/rapiddweller/format/script/ScriptUtil.java
@@ -20,7 +20,6 @@ import com.rapiddweller.common.ConfigurationError;
 import com.rapiddweller.common.Context;
 import com.rapiddweller.common.FileUtil;
 import com.rapiddweller.common.IOUtil;
-import com.rapiddweller.common.LogCategoriesConstants;
 import com.rapiddweller.common.StringUtil;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
@@ -31,16 +30,13 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.WeakHashMap;
 
-
 /**
  * Utility class for scripting.
- *
  * @author Volker Bergmann
  * @since 0.3.0
  */
 public class ScriptUtil {
 
-  private static final Logger CONFIG_LOGGER = LoggerFactory.getLogger(LogCategoriesConstants.CONFIG);
   private static final Logger SCRIPTUTIL_LOGGER = LoggerFactory.getLogger(ScriptUtil.class);
 
   // extension mapping -----------------------------------------------------------------------------------------------
@@ -55,51 +51,16 @@ public class ScriptUtil {
     parseConfigFile();
   }
 
+  private ScriptUtil() {
+    // private constructor to prevent instantiation
+  }
+
   // utility methods -------------------------------------------------------------------------------------------------
 
-  /**
-   * Execute object.
-   *
-   * @param script  the script
-   * @param context the context
-   * @return the object
-   */
   public static Object execute(Script script, Context context) {
     return script.evaluate(context);
   }
 
-    /*    
-    public static void execute(String filename, Writer out,
-            String variableName, Object variableValue) throws IOException,
-            ScriptException {
-        Context context = new DefaultContext();
-        context.set(variableName, variableValue);
-        execute(filename, context, out);
-    }
-
-    public static void execute(String filename, Context context, Writer out)
-            throws IOException, ScriptException {
-        Script script = getInstance(filename);
-        script.execute(context, out);
-    }
-
-    public static void execute(String filename, Writer out,
-            ScriptVariable... variables) throws IOException, ScriptException {
-        Script script = getInstance(filename);
-        Context context = new DefaultContext();
-        for (ScriptVariable variable : variables)
-            context.set(variable.getName(), variable.getValue());
-        script.execute(context, out);
-    }
-*/
-
-  /**
-   * Evaluate object.
-   *
-   * @param text    the text
-   * @param context the context
-   * @return the object
-   */
   public static Object evaluate(String text, Context context) {
     String trimmedText = text.trim();
     if (trimmedText.startsWith("{{") && trimmedText.endsWith("}}")) {
@@ -116,30 +77,15 @@ public class ScriptUtil {
 
   private static final Map<String, Script> scriptsByName = new WeakHashMap<>();
 
-  /**
-   * Read file script.
-   *
-   * @param uri the uri
-   * @return the script
-   * @throws IOException the io exception
-   */
   public static Script readFile(String uri) throws IOException {
     Script script = scriptsByName.get(uri);
     if (script == null) {
-      String engineId = FileUtil.suffix(uri);
-      ScriptFactory factory = getFactory(engineId, true);
-      script = factory.readFile(uri);
+      script = factoryForUri(uri).readFile(uri);
       scriptsByName.put(uri, script);
     }
     return script;
   }
 
-  /**
-   * Parse unspecific text script.
-   *
-   * @param text the text
-   * @return the script
-   */
   public static Script parseUnspecificText(String text) {
     if (isScript(text)) {
       return parseScriptText(text.substring(1, text.length() - 1), false);
@@ -147,12 +93,6 @@ public class ScriptUtil {
     return new ConstantScript(text);
   }
 
-  /**
-   * Is script boolean.
-   *
-   * @param text the text
-   * @return the boolean
-   */
   public static boolean isScript(String text) {
     if (StringUtil.isEmpty(text)) {
       return false;
@@ -161,23 +101,10 @@ public class ScriptUtil {
     return trimmedText.startsWith("{") && trimmedText.endsWith("}");
   }
 
-  /**
-   * Parse script text script.
-   *
-   * @param text the text
-   * @return the script
-   */
   public static Script parseScriptText(String text) {
     return parseScriptText(text, true);
   }
 
-  /**
-   * Parse script text script.
-   *
-   * @param text           the text
-   * @param removeBrackets the remove brackets
-   * @return the script
-   */
   public static Script parseScriptText(String text, boolean removeBrackets) {
     if (text == null) {
       return null;
@@ -195,13 +122,6 @@ public class ScriptUtil {
     }
   }
 
-  /**
-   * Parse script text script.
-   *
-   * @param text     the text
-   * @param engineId the engine id
-   * @return the script
-   */
   public static Script parseScriptText(String text, String engineId) {
     if (engineId == null) {
       throw new IllegalArgumentException("engineId is null");
@@ -214,30 +134,14 @@ public class ScriptUtil {
     }
   }
 
-  /**
-   * Add factory.
-   *
-   * @param name    the name
-   * @param factory the factory
-   */
   public static void addFactory(String name, ScriptFactory factory) {
     factories.put(name, factory);
   }
 
-  /**
-   * Gets default script engine.
-   *
-   * @return the default script engine
-   */
   public static String getDefaultScriptEngine() {
     return defaultScriptEngine;
   }
 
-  /**
-   * Sets default script engine.
-   *
-   * @param defaultScriptEngine the default script engine
-   */
   public static void setDefaultScriptEngine(String defaultScriptEngine) {
     if (factories.get(defaultScriptEngine) == null) {
       throw new RuntimeException("Unknown script engine id: " + defaultScriptEngine);
@@ -245,12 +149,6 @@ public class ScriptUtil {
     ScriptUtil.defaultScriptEngine = defaultScriptEngine;
   }
 
-  /**
-   * Combine scriptable parts string.
-   *
-   * @param parts the parts
-   * @return the string
-   */
   public static String combineScriptableParts(String... parts) {
     String scriptEngine = getCommonScriptEngine(parts);
     boolean template = ("ftl".equals(scriptEngine));
@@ -280,12 +178,11 @@ public class ScriptUtil {
 
   // private helpers -------------------------------------------------------------------------------------------------
 
-  /**
-   * Gets common script engine.
-   *
-   * @param parts the parts
-   * @return the common script engine
-   */
+  private static ScriptFactory factoryForUri(String uri) {
+    String engineId = FileUtil.suffix(uri);
+    return getFactory(engineId, true);
+  }
+
   static String getCommonScriptEngine(String... parts) {
     ScriptDescriptor[] descriptors = describe(parts);
     for (int i = 0; i < parts.length; i++) {
@@ -296,12 +193,6 @@ public class ScriptUtil {
     return null;
   }
 
-  /**
-   * Describe script descriptor [ ].
-   *
-   * @param parts the parts
-   * @return the script descriptor [ ]
-   */
   static ScriptDescriptor[] describe(String... parts) {
     ScriptDescriptor[] descriptors = new ScriptDescriptor[parts.length];
     for (int i = 0; i < parts.length; i++) {
@@ -310,13 +201,6 @@ public class ScriptUtil {
     return descriptors;
   }
 
-  /**
-   * Gets factory.
-   *
-   * @param engineId the engine id
-   * @param required the required
-   * @return the factory
-   */
   static ScriptFactory getFactory(String engineId, boolean required) {
     ScriptFactory factory = factories.get(engineId);
     if (factory == null && required) {
@@ -329,8 +213,6 @@ public class ScriptUtil {
     String className;
     try {
       factories = new HashMap<>();
-//            org.graalvm.polyglot.Engine graalVMEngine = Engine.newBuilder().build();
-//            CONFIG_LOGGER.info("supported script languages GraalVM : " + graalVMEngine.getLanguages().keySet());
       // read config file
       SCRIPTUTIL_LOGGER.debug("Initializing Script mapping from file " + SETUP_FILE_NAME);
       Map<String, String> properties = IOUtil.readProperties(SETUP_FILE_NAME);
