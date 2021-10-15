df6f0b4545a2e62bcfe34e2e033bda791ec2199d
Volker Bergmann <volker.der.bergmann@gmx.de>
2021-10-15 07:40:44 +0200

    removed auto-generated JavaDoc

--- src/main/java/com/rapiddweller/format/xsd/ComplexType.java
+++ src/main/java/com/rapiddweller/format/xsd/ComplexType.java
@@ -23,50 +23,26 @@ import java.util.Map;
 /**
  * Parent class for schema elements that represent complex types.
  * Created: 16.05.2014 19:21:44
- *
  * @author Volker Bergmann
  * @since 0.8.2
  */
 public abstract class ComplexType extends NamedSchemaElement {
 
-  /**
-   * The Attributes.
-   */
   protected Map<String, Attribute> attributes;
 
-  /**
-   * Instantiates a new Complex type.
-   *
-   * @param name the name
-   */
-  public ComplexType(String name) {
+  protected ComplexType(String name) {
     super(name);
     this.attributes = new OrderedNameMap<>();
   }
 
-  /**
-   * Add attribute.
-   *
-   * @param attribute the attribute
-   */
   public void addAttribute(Attribute attribute) {
     this.attributes.put(attribute.getName(), attribute);
   }
 
-  /**
-   * Gets attributes.
-   *
-   * @return the attributes
-   */
   public Collection<Attribute> getAttributes() {
     return attributes.values();
   }
 
-  /**
-   * Print content.
-   *
-   * @param string the string
-   */
   public abstract void printContent(String string);
 
 }
